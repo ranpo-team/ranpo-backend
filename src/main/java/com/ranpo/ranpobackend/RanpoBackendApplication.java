6212be9d637254da14eefd48337694be8ca0abf5
subbni <slfkak@naver.com>
2025-05-17 14:48:33 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/com/ranpo/ranpobackend/RanpoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ranpo.ranpobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanpoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanpoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ranpo/ranpobackend/RanpoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranpo.ranpobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanpoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
